508956423687b24f0c76fe30a1823ef90b4b159b
Zhao Hui Benjamin Koh <bkben64@gmail.com>
2025-05-19 12:55:27 +0200

    Stash

--- thirdparty/gaussian_splatting/scene/gaussian_model.py
+++ thirdparty/gaussian_splatting/scene/gaussian_model.py
@@ -316,9 +316,9 @@ class GaussianModel:
         ]
 
         if training_args.optimizer == "sparse_adam":
-            self.optimizer = SparseGaussianAdam(l, lr=0.0, eps=1e-15)
-        # else:
-        #     self.optimizer = torch.optim.Adam(l, lr=0.0, eps=1e-15)
+            self.optimizer = torch.optim.SparseAdam(l, lr=1e-15, eps=1e-15)
+        else:
+            self.optimizer = torch.optim.SparseAdam(l, lr=1e-15, eps=1e-15)
         self.xyz_scheduler_args = get_expon_lr_func(
             lr_init=training_args.position_lr_init * self.spatial_lr_scale,
             lr_final=training_args.position_lr_final * self.spatial_lr_scale,
